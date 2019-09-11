b9fb46686502cf0ab7f3ffdde2eb15ed855ecef4
davidemvf <davidemv.fiore@gmail.com>
2019-09-12 01:55:39 +0200

    step 3 in corso, api completato, aggiungere i grafici

--- apiTre.php
+++ apiTre.php
@@ -10,10 +10,10 @@ if ($_GET['level'] == 'guest'){
         $data['fatturato'] = $graphs['fatturato'];
       }
      else if ($_GET['level'] == 'employee'){
-       // foreach ($graphs as $level => $data) {
+
          $data['fatturato'] = $graphs['fatturato'];
          $data['fatturato_by_agent'] = $graphs['fatturato_by_agent'];
-       // }
+      
       }
      else if ($_GET['level'] == 'clevel') {
        $data['fatturato'] = $graphs['fatturato'];
